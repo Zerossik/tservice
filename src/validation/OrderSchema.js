514d40f5a1a12dac106e756f83a5fb0a4ca9f254
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-12-16 12:56:35 +0000

    test_6

--- src/validation/OrderSchema.js
+++ src/validation/OrderSchema.js
@@ -1,18 +1,14 @@
 import * as Yup from "yup";
-// import "yup-phone-lite";
 
 export const OrderSchema = Yup.object().shape({
-  type: Yup.string().required("Required"),
-  manufacturer: Yup.string().required("Required"),
-  model: Yup.string().required("Required"),
-  deviceID: Yup.string(),
-  customerName: Yup.string().required("Required"),
-  // phoneNumber: Yup.string()
-  //   .phone("UA", "Please enter a valid phone number")
-  //   .required("Required"),
-  price: Yup.number().required("Required"),
-  status: Yup.string().required("Required"),
-  masterName: Yup.string(),
-  description: Yup.string(),
-  failure: Yup.string(),
+  password: Yup.string()
+    .min(6, "Password must be 6 characters long")
+    // .matches(/[0-9]/, "Password requires a number")
+    // .matches(/[a-z]/, "Password requires a lowercase letter")
+    // .matches(/[A-Z]/, "Password requires an uppercase letter")
+    // .matches(/[^\w]/, "Password requires a symbol")
+    .required("Required"),
+  repeatPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords don't match")
+    .required("Required"),
 });
--- src/validation/index.js
+++ src/validation/index.js
@@ -3,4 +3,4 @@ export { SigninSchema } from "./signinSchema";
 export { ResetPassSchema } from "./resetPassSchema";
 export { NewPassSchema } from "./newPassSchema";
 // export { OrderSchema } from "./orderSchema";
-export { OrderSchema } from "./newTestSchema";
+export { OrderSchema } from "./orderSchema";

8b9c3ac6b2ef26ad119883f7c3b0092ed92b1ea5
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-12-16 12:14:47 +0000

    test_2

--- src/validation/index.js
+++ src/validation/index.js
@@ -2,4 +2,5 @@ export { SignupSchema } from "./signupSchema";
 export { SigninSchema } from "./signinSchema";
 export { ResetPassSchema } from "./resetPassSchema";
 export { NewPassSchema } from "./newPassSchema";
-export { OrderSchema } from "./orderSchema";
+// export { OrderSchema } from "./orderSchema";
+export { NewTestSchema } from "./newTestSchema";
